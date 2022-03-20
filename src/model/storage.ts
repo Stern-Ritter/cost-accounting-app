50370986c106fb78ee07958faaabcd4ed88900e1
Stern-Ritter <fadeew.a@mail.ru>
2022-03-20 19:34:01 +0300

    Refactor code

--- src/model/storage.ts
+++ src/model/storage.ts
@@ -1,13 +1,20 @@
-import { categoryCollectionName, transactionCollectionName, firebaseConfig } from "../utils/api";
 import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import {
+  categoryCollectionName,
+  transactionCollectionName,
+  firebaseConfig,
+} from "../utils/api";
 import FirebaseCategoryModel from "./category/FireBaseCategoryModel";
 import FirebaseTransactionModel from "./transaction/FireBaseTransactionModel";
-import { initializeApp } from "firebase/app";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const categoryStorage = new FirebaseCategoryModel(db, categoryCollectionName);
-const transactionStorage = new FirebaseTransactionModel(db, transactionCollectionName);
+const transactionStorage = new FirebaseTransactionModel(
+  db,
+  transactionCollectionName
+);
 
 export { categoryStorage, transactionStorage };
