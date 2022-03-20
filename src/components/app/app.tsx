82b1e09097fbba6cb6ad0f93d2a576bb497de8f7
Stern-Ritter <fadeew.a@mail.ru>
2022-03-20 17:58:21 +0300

    Add index.tsx, store.ts, App component

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import styles from "./app.module.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch></Switch>
+    </Router>
+  );
+}
+
+export default App;
