1a7e7a2b17b2648cc059a8fea7a8857cc74ede09
Stern-Ritter <fadeew.a@mail.ru>
2022-04-07 23:58:54 +0300

    Edit Create user form, Authentication form error messages

--- src/components/authentication-form/authentication-form.tsx
+++ src/components/authentication-form/authentication-form.tsx
@@ -38,7 +38,7 @@ function AuthenticationForm() {
         history.push({ pathname: "/" });
       })
       .catch((err) => {
-        dispatch(setAuthenticationFormErrorMessage(err.message));
+        dispatch(setAuthenticationFormErrorMessage({text: err.message}));
       });
   };
 
